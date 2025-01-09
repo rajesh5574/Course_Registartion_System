c8119768032ee77b551b7571db098346234ba469
rajesh5574 <rajeshrocky5574@gmail.com>
2025-01-09 20:31:05 +0530

    backend files added

---
+++ Course-Registration-System/src/main/java/com/example/Course/Registration/System/CourseRegistrationSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.Course.Registration.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CourseRegistrationSystemApplication.class, args);
+	}
+
+}
