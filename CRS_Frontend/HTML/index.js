7c66f06d4d50c65f3513ef27c9a36fc4424e81d8
rajesh5574 <rajeshrocky5574@gmail.com>
2025-01-10 19:20:20 +0530

    files are added

---
+++ CRS_Frontend/HTML/index.js
@@ -0,0 +1,32 @@
+function showCourses(){
+    fetch("http://localhost:8080/courses")
+    .then((response) => response.json())
+    .then((courses) => {
+        const dataTable = document.getElementById("courseTable")
+        courses.forEach(course => {
+            var row = `<tr>
+                <td>${course.courseId}</td>
+                <td>${course.courseName}</td>
+                <td>${course.trainer}</td>
+                <td>${course.durationInWeeks}</td>
+            </tr>`
+            dataTable.innerHTML+=row;
+        });
+    });
+}
+
+function showStudents(){
+    fetch("http://localhost:8080/courses/enrolled")
+    .then((response) => response.json())
+    .then((students) => {
+        const dataTable = document.getElementById("enrolledTable")
+        students.forEach(student => {
+            var row = `<tr>
+                <td>${student.name}</td>
+                <td>${student.emailId}</td>
+                <td>${student.courseName}</td>
+            </tr>`
+            dataTable.innerHTML+=row;
+        });
+    });
+}
